c4e71dc2599db35362093843465524f9dc18e293
sukerin <563302070@qq.com>
2020-08-10 23:28:40 +0800

    complete

--- HeiHeiZhang/Form1.Designer.cs
+++ HeiHeiZhang/Form1.Designer.cs
@@ -28,57 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.text_shortNum = new System.Windows.Forms.TextBox();
+            this.text_short_length = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button_push = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.text_long_length = new System.Windows.Forms.TextBox();
+            this.label_about = new System.Windows.Forms.Label();
+            this.button_clean = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(463, 259);
+            this.button1.Location = new System.Drawing.Point(52, 349);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(169, 47);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "计算";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // textBox1
+            // text_shortNum
             // 
-            this.textBox1.Location = new System.Drawing.Point(258, 271);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 1;
+            this.text_shortNum.Location = new System.Drawing.Point(121, 107);
+            this.text_shortNum.Name = "text_shortNum";
+            this.text_shortNum.Size = new System.Drawing.Size(100, 21);
+            this.text_shortNum.TabIndex = 1;
             // 
-            // textBox2
+            // text_short_length
             // 
-            this.textBox2.Location = new System.Drawing.Point(106, 259);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 21);
-            this.textBox2.TabIndex = 2;
+            this.text_short_length.Location = new System.Drawing.Point(121, 80);
+            this.text_short_length.Name = "text_short_length";
+            this.text_short_length.Size = new System.Drawing.Size(100, 21);
+            this.text_short_length.TabIndex = 2;
             // 
-            // button2
+            // label1
             // 
-            this.button2.Location = new System.Drawing.Point(363, 214);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "短棒子长度";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "数量";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(52, 194);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(169, 136);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "长度";
+            this.columnHeader1.Width = 80;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "数量";
+            this.columnHeader2.Width = 80;
+            // 
+            // button_push
+            // 
+            this.button_push.Font = new System.Drawing.Font("宋体", 9F);
+            this.button_push.Location = new System.Drawing.Point(52, 144);
+            this.button_push.Name = "button_push";
+            this.button_push.Size = new System.Drawing.Size(79, 37);
+            this.button_push.TabIndex = 7;
+            this.button_push.Text = "压入";
+            this.button_push.UseVisualStyleBackColor = true;
+            this.button_push.Click += new System.EventHandler(this.Button_push_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "总长度";
+            // 
+            // text_long_length
+            // 
+            this.text_long_length.Location = new System.Drawing.Point(121, 37);
+            this.text_long_length.Name = "text_long_length";
+            this.text_long_length.Size = new System.Drawing.Size(100, 21);
+            this.text_long_length.TabIndex = 8;
+            // 
+            // label_about
+            // 
+            this.label_about.AutoSize = true;
+            this.label_about.Location = new System.Drawing.Point(230, 429);
+            this.label_about.Name = "label_about";
+            this.label_about.Size = new System.Drawing.Size(29, 12);
+            this.label_about.TabIndex = 10;
+            this.label_about.Text = "关于";
+            this.toolTip1.SetToolTip(this.label_about, "copyright by sukerin");
+            // 
+            // button_clean
+            // 
+            this.button_clean.Font = new System.Drawing.Font("宋体", 9F);
+            this.button_clean.Location = new System.Drawing.Point(142, 144);
+            this.button_clean.Name = "button_clean";
+            this.button_clean.Size = new System.Drawing.Size(79, 37);
+            this.button_clean.TabIndex = 11;
+            this.button_clean.Text = "清空";
+            this.button_clean.UseVisualStyleBackColor = true;
+            this.button_clean.Click += new System.EventHandler(this.Button_clean_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 5;
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 5;
+            this.toolTip1.ReshowDelay = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(271, 450);
+            this.Controls.Add(this.button_clean);
+            this.Controls.Add(this.label_about);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.text_long_length);
+            this.Controls.Add(this.button_push);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text_short_length);
+            this.Controls.Add(this.text_shortNum);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "嘿味";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,9 +192,19 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox text_shortNum;
+        private System.Windows.Forms.TextBox text_short_length;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button_push;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox text_long_length;
+        private System.Windows.Forms.Label label_about;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button_clean;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
--- HeiHeiZhang/Form1.cs
+++ HeiHeiZhang/Form1.cs
@@ -12,9 +12,13 @@ namespace HeiHeiZhang
 {
     public partial class Form1 : Form
     {
+        private float packageCapacity;
+        private List<Food> foodList = new List<Food>();
+
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void calPath(int[,] f)
@@ -24,9 +28,42 @@ namespace HeiHeiZhang
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            int packageCapacity = 800;
-            int[] w = { 0, 200, 300, 400, 500 };
-            int[] v = { 0, 301, 401, 602, 601 };
+            if (foodList.Count == 0)
+            {
+                MessageBox.Show("请输入数据");
+                return;
+            }
+            //int packageCapacity = 800;
+            //int[] w = { 0, 200, 300, 400, 500 };
+            //int[] v = { 0, 301, 401, 602, 601 };
+            int packageCapacity;
+            try
+            {
+                packageCapacity = Convert.ToInt32((float.Parse(text_long_length.Text) * 100));
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("长度请输入数字（最多支持2位小数）");
+                return;
+            }
+
+
+
+            int[] w = new int[foodList.Count + 1];
+            int[] v = new int[foodList.Count + 1];
+            foodList.OrderBy(p => p.value).ToList();
+            w[0] = 0;
+            v[0] = 0;
+            int index = 1;
+            foreach (var item in foodList)
+            {
+                w[index] = Convert.ToInt32((item.weight * 100));
+                v[index] = Convert.ToInt32((item.value * 100));
+                index++;
+            }
+
+
             int goodsNum = w.Length;
             int[,] f = new int[goodsNum, packageCapacity + 1];
             List<int> path = new List<int>();
@@ -46,14 +83,15 @@ namespace HeiHeiZhang
                 }
             }
 
-            int f0 = goodsNum-1;
+            int f0 = goodsNum - 1;
             int f1 = packageCapacity;
-            
-            while (f0 > 0&&f1 > 0){
+
+            while (f0 > 0 && f1 > 0)
+            {
                 if (w[f0] > f1)
                 {
                     f0 = f0 - 1;
-                  
+
                 }
                 else
                 {
@@ -75,8 +113,21 @@ namespace HeiHeiZhang
                 }
             }
 
+
+            float total = f[goodsNum - 1, packageCapacity]/100f;
+            float remain = (packageCapacity - f[goodsNum - 1, packageCapacity])/100f;
            
-            foreach (int str in path) Console.WriteLine(str);
+            List<string> pathWeight = new List<string>(); 
+            foreach(int p in path)
+            {
+                pathWeight.Add(foodList[p-1].weight.ToString()+"短棒*1");
+            }
+            pathWeight.Insert(0, "耗材如下:");
+            string pathNote = string.Join("\n", pathWeight.ToArray());
+
+            MessageBox.Show(string.Format("总计耗费{0}\n剩余{1}\n{2}", total,remain,pathNote));
+
+            //foreach (int str in path) Console.WriteLine(str);
 
             Console.WriteLine("f[{0},{1}]-{2} ", goodsNum - 1, packageCapacity, f[goodsNum - 1, packageCapacity]);
             /*打印*/
@@ -96,9 +147,50 @@ namespace HeiHeiZhang
 
         }
 
-        private void Button2_Click(object sender, EventArgs e)
+
+
+        private void Button_push_Click(object sender, EventArgs e)
         {
+            //float long_length = float.Parse(text_long_length.Text);
+            try
+            {
+                float short_length = float.Parse(float.Parse(text_short_length.Text).ToString("f2"));
+                int short_num = int.Parse(text_shortNum.Text);
+                ListViewItem item = new ListViewItem();
+                item.Text = text_short_length.Text;
+                item.SubItems.Add(text_shortNum.Text);
+              
+
+                listView1.Items.Add(item);
+                for (int i = 0; i < short_num; i++)
+                {
+                    foodList.Add(new Food(short_length, short_length));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("长度请填写数字（最多支持2位小数），数量请填写整数");
+            }
 
         }
+        private void Button_clean_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            foodList.Clear();
+        }
+        public class Food
+
+        {
+            public float weight;
+            public float value;
+
+            public Food(float weight, float value)
+            {
+                this.weight = weight;
+                this.value = value;
+            }
+        }
+
+      
     }
 }
